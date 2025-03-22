ab0409494a1de724be132ff3bc23fd80b7e9906f
jakubek16cm <kuba.stanczyk201@gmail.com>
2025-03-22 10:40:38 +0000

    2_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,26 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Wpisz wartość: ");
+    int num1 = scanner.nextInt();
+
+
+    System.out.println("Wpisz wartość: ");
+    int num2 = scanner.nextInt();
+
+    System.out.println("Wpisz wartość: ");
+    int num3 = scanner.nextInt();
+
+    int max = Math.max(num1, Math.max(num2, num3));
+
+    System.out.println("Największa liczba to: " + max);
+
+    scanner.close();
+    
   }
 
   // @Test
